5d4c328d16d44852e488f577ff14bc08f8036f6b
joyadeep <dev.joyadeep@gmail.com>
2025-07-08 15:04:21 +0545

    implementation of orthographic camero
    fix: scene auto adjust to screen change
    
    anitaliasing

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import {OrbitControls} from 'three/addons/controls/OrbitControls.js'
 
 const scene = new THREE.Scene();
 
@@ -9,13 +10,34 @@ scene.add(cubeMesh);
 
 // adding a camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1 , 30);
+// const aspectRatio = window.innerWidth/ window.innerHeight;
+// const camera = new THREE.OrthographicCamera(-1*aspectRatio,1*aspectRatio,1,-1,0.1,200)
+
 camera.position.z = 5;
 scene.add(camera);
 
 
 const canvas = document.querySelector('canvas.threejs')
 const renderer = new THREE.WebGLRenderer({
-  canvas
+  canvas,
+  antialias: true
 })
 renderer.setSize(window.innerWidth,window.innerHeight)
-renderer.render(scene,camera)
+
+const controls = new OrbitControls(camera,canvas);
+controls.enableDamping = true;
+controls.autoRotate = true;
+
+window.addEventListener('resize',()=>{
+  camera.aspect = window.innerWidth/window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth,window.innerHeight);
+})
+
+const renderloop = () => {
+  controls.update();
+  renderer.render(scene,camera);
+  window.requestAnimationFrame(renderloop);
+}
+
+renderloop()
