7b6fa1bc8ed870d82d959278b43e8bddf8f8100b
joyadeep <dev.joyadeep@gmail.com>
2025-07-11 11:09:33 +0545

    add: grouping

--- src/main.js
+++ src/main.js
@@ -6,16 +6,24 @@ const scene = new THREE.Scene();
 const cubeGeometry = new THREE.BoxGeometry(1,1,1);
 const cubeMaterial = new THREE.MeshBasicMaterial({color: "red"})
 const cubeMesh = new THREE.Mesh(cubeGeometry,cubeMaterial)
-scene.add(cubeMesh);
+cubeMesh.position.y= -1;
+const cubeMesh2 = new THREE.Mesh(cubeGeometry,cubeMaterial)
+cubeMesh2.position.x=2;
+const cubeMesh3 = new THREE.Mesh(cubeGeometry,cubeMaterial)
+cubeMesh3.position.x=-2;
+// scene.add(cubeMesh);
 
-const position = new THREE.Vector3(1,1,2);
-cubeMesh.position.copy(position);
-// cubeMesh.scale.x = 3;
-cubeMesh.scale.set(3,2,2)
+// const position = new THREE.Vector3(1,1,2);
+// cubeMesh.position.copy(position);
 
-// cubeMesh.position.y = 1;
-// cubeMesh.position.x = 1;
-// cubeMesh.position.z = 1;
+const group = new THREE.Group();
+group.position.setScalar(1)
+group.add(cubeMesh);
+group.add(cubeMesh2);
+group.add(cubeMesh3);
+scene.add(group);
+
+// cubeMesh.scale.set(3,2,2)
 
 const axesHelper = new THREE.AxesHelper(5);
 
@@ -34,8 +42,6 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(window.innerWidth,window.innerHeight)
 const controls = new OrbitControls(camera,canvas);
 controls.enableDamping = true;
-// controls.autoRotate = true;
-// controls.rotateSpeed = 100;
 const renderloop = () =>{
   controls.update();
   renderer.render(scene,camera)
